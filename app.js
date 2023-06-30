f5ad05c392f80f0d6ad308eb3d60363449a0129b
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-06-30 01:48:38 -0500

    Command File System

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
-
-const language = require('./langOptions');
-const translate = require('google-translate-api');
+//const language = require('./langOptions');
+//const translate = require('google-translate-api');
 //const speech = require('./messages');
 
 require('dotenv').config();
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once('ready', c => {
+client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = interaction.client.commands.get(interaction.commandName);
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 /*
 client.on("message", (message) => {
@@ -85,3 +122,6 @@ client.on("message", (message) => {
 
 client.login(process.env.BOT_TOKEN);
 */
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_BOT_TOKEN);
