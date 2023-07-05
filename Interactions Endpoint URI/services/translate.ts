b7b05a15298df9488dae0a2e556a596217fda0fa
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-07-05 11:07:18 -0500

    Fix Indicies

--- Interactions Endpoint URI/services/translate.ts
+++ Interactions Endpoint URI/services/translate.ts
@@ -5,7 +5,7 @@ const transURI = "https://translation-service-kappa.vercel.app/api/translate?";
 export async function TranslateContent(interaction_data: APIApplicationCommandInteractionDataOption[]): Promise<IBXTranslationObject>
 {
     const selectedLanguage = interaction_data[0] as APIApplicationCommandInteractionDataStringOption;
-    const content = interaction_data[0] as APIApplicationCommandInteractionDataStringOption;
+    const content = interaction_data[1] as APIApplicationCommandInteractionDataStringOption;
 
     const response = await fetch(transURI + new URLSearchParams({
         selectedLanguage: selectedLanguage.value,
