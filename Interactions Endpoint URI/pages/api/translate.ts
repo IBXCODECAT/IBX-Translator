112b759a465e046cb3008a5443a1b3f1bfdbb4e2
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-07-05 00:49:06 -0500

    Translations Service

---
+++ Interactions Endpoint URI/pages/api/translate.ts
@@ -0,0 +1,28 @@
+import translate from 'google-translate-api';
+
+export default async function handler(req: any, res: any) {
+    //const data = await GetDataTest();
+
+    const settings = { from: undefined, to: "ru" }
+
+    //const translation = await TranslationTest("ru", "hello!");
+    
+    let translation = "null";
+
+    
+    await translate("hi", settings).then(translateRes => {
+        console.log(translateRes.text);
+        translation = translateRes.text;
+    })
+
+    
+    return res.status(200).json( { data: translation });
+    
+
+}
+
+async function GetDataTest(): Promise<string> {
+    const res = await fetch("	https://api.adviceslip.com/advice/0");
+
+    return "eggs";
+}
