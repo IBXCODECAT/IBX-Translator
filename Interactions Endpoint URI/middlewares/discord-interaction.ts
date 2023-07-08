7df2ad10aef9342850fdfd1502e42add6757c2dd
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-07-07 21:24:19 -0500

    DEFER res + MULTIPLE RESPONSES

--- Interactions Endpoint URI/middlewares/discord-interaction.ts
+++ Interactions Endpoint URI/middlewares/discord-interaction.ts
@@ -1,4 +1,4 @@
-import { APIApplicationCommandInteraction, InteractionType } from "discord-api-types/v10"
+import { APIApplicationCommandInteraction, InteractionResponseType, InteractionType, MessageFlags } from "discord-api-types/v10"
 import { DiscordInteractionApiHandler } from "./../interfaces/discord"
 import { NextApiRequest, NextApiResponse } from "next"
 import nacl from "tweetnacl"
@@ -6,6 +6,7 @@ import { parseRawBodyAsString } from "./../utils/body-parser"
 
 // Your public key can be found on your application in the Developer Portal
 const DISCORD_APP_PUBLIC_KEY = process.env.DISCORD_APP_PUBLIC_KEY
+const DISCORD_APP_ID = process.env.CLIENT_ID;
 
 if (!DISCORD_APP_PUBLIC_KEY) {
   throw new Error("Environment variables not configured correctly")
@@ -33,7 +34,7 @@ export const verifyHeaders = ({ timestamp, rawBody, signature }: VerifyHeadersAr
  * Middleware to verify the validity of the incoming webhook request according to https://discord.com/developers/docs/interactions/slash-commands#security-and-authorization
  * When using this middleware, your API route handler must disable body parsing
  */
-const withDiscordInteraction = (next: DiscordInteractionApiHandler) => async (
+export const withDiscordInteraction = (next: DiscordInteractionApiHandler) => async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
@@ -69,4 +70,34 @@ const withDiscordInteraction = (next: DiscordInteractionApiHandler) => async (
   }
 }
 
-export default withDiscordInteraction
+
+const INTERACTION_DEFER_RESPONSE = { type: InteractionResponseType.DeferredChannelMessageWithSource as number }
+const INTERACTION_EDIT_RESPONSE = { type: InteractionResponseType.UpdateMessage as number }
+
+export async function DeferSlashCommandResponse(interaction: APIApplicationCommandInteraction, message_flags: MessageFlags)
+{
+  const fetchOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ...INTERACTION_DEFER_RESPONSE, data: { flags: message_flags } })
+  };
+
+  const response = await fetch(`https://discord.com/api/v10/interactions/${interaction.id}/${interaction.token}/callback`, fetchOptions);
+
+  console.log(JSON.stringify(await response));
+}
+
+export async function EditSlashCommandResponse(interaction: APIApplicationCommandInteraction, message_content: string, message_embeds?: any[]) {
+  const fetchOptions = {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content: message_content, embeds: message_embeds })
+    //body: JSON.stringify({ ...INTERACTION_EDIT_RESPONSE, data: { tts: false, content: payload, embeds: message_embeds } })
+  }
+
+  console.log(DISCORD_APP_ID);
+
+  const response = await fetch(`https://discord.com/api/v10/webhooks/${DISCORD_APP_ID}/${interaction.token}/messages/@original`, fetchOptions);
+
+  console.log(JSON.stringify(await response));
+}
--- Interactions Endpoint URI/pages/api/interactions/index.ts
+++ Interactions Endpoint URI/pages/api/interactions/index.ts
@@ -1,26 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { APIApplicationCommandInteraction, APIChatInputApplicationCommandInteraction, APIInteractionResponse, InteractionResponseType, MessageFlags } from "discord-api-types/v10"
-import withDiscordInteraction from "./../../../middlewares/discord-interaction"
+import { DeferSlashCommandResponse, EditSlashCommandResponse, withDiscordInteraction } from "./../../../middlewares/discord-interaction"
 import withErrorHandler from "./../../../middlewares/error-handler"
 import { TranslateContent } from "../../../services/translate";
 import { IBXTranslationObject as IBXTranslationObject } from "../../../interfaces/transObj";
 import { HELP_EMBED } from "../../../resources/embeds";
+import { NextResponse } from "next/server";
 
 const INTERACTION_ACK_PING = { type: InteractionResponseType.Pong as number };
 const INTERACTION_RESPOND_INSTANTLY = { type: InteractionResponseType.ChannelMessageWithSource as number }
 const INTERACTION_ACK = { type: InteractionResponseType.DeferredChannelMessageWithSource as number }
-const INTERACTION_RESPOND_LATE = { type: InteractionResponseType.DeferredMessageUpdate as number }
 const INTERACTION_ACK_WITH_INPUT_MODAL = { type: InteractionResponseType.Modal as number }
+const INTERACTION_EDIT_RESPONSE = { type: InteractionResponseType.UpdateMessage as number }
 
-const COMMAND_RESPONSE_DATA = { tts: false, flags: MessageFlags.Ephemeral }
+const PRIVATE_COMMAND_RESPONSE_DATA = { tts: false, flags: MessageFlags.Ephemeral }
+const PUBLIC_COMMAND_RESPONSE_DATA = { tts: false, flags: 0 }
 
-const ACK_INTERACTION = { ...INTERACTION_ACK, data: { ...COMMAND_RESPONSE_DATA } };
+const ACK_INTERACTION = { ...INTERACTION_ACK, data: { ...PRIVATE_COMMAND_RESPONSE_DATA } };
 
-const INVALID_COMMAND_RESPONSE = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...COMMAND_RESPONSE_DATA, content: "You have executed a command that does not exist in my directory. Try again later or run **/support** to contact the developers." } }
+const INVALID_COMMAND_RESPONSE = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...PRIVATE_COMMAND_RESPONSE_DATA, content: "You have executed a command that does not exist in my directory. Try again later or run **/support** to contact the developers." } }
 
-const COMMAND_SUPPORT_RESPONSE = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...COMMAND_RESPONSE_DATA, content: "https://discord.gg/Zy5uXQUZXx" }}
+const COMMAND_SUPPORT_RESPONSE = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...PRIVATE_COMMAND_RESPONSE_DATA, content: "https://discord.gg/Zy5uXQUZXx" }}
 
-const COMMAND_HELP_RESPONSE = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...COMMAND_RESPONSE_DATA, embeds: HELP_EMBED }}
+const COMMAND_HELP_RESPONSE = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...PUBLIC_COMMAND_RESPONSE_DATA, embeds: HELP_EMBED }}
 
 // disable body parsing, need the raw body as per https://discord.com/developers/docs/interactions/slash-commands#security-and-authorization
 export const config = {
@@ -50,21 +52,21 @@ const handler = async (
         switch(options![0].name)
         {
           case 'help':
-            const response_help = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...COMMAND_RESPONSE_DATA, content: 'There will be a help message here someday, and that day is not today...' } }
+            const response_help = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...PRIVATE_COMMAND_RESPONSE_DATA, content: 'There will be a help message here someday, and that day is not today...' } }
             return res.status(200).json(response_help);
+          
           case 'message':
-            const translation_msg: IBXTranslationObject = await TranslateContent(options!)
-            const ans_msg = translation_msg.translation;
-            const response_msg = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...COMMAND_RESPONSE_DATA, content: `Here is a preview of your translated message:\n\n\`${ans_msg}\`\n\nA message will be sent in this channel shortly...` }}
-
-            //SEND A MESSAGE IN THE EXECUTION CHANNEL HERE!!!
-
-            return res.status(200).json(response_msg);
+            await DeferSlashCommandResponse(interaction, MessageFlags.Ephemeral);
+            const translation_msg: IBXTranslationObject = await TranslateContent(options!);
+            await EditSlashCommandResponse(interaction, `**<@${interaction.member?.user.id}> says:** \`${translation_msg.translation}\``);
+            return res.status(200).json(ACK_INTERACTION);
+          
           case 'preview':
-            const translation_preview: IBXTranslationObject = await TranslateContent(options!)
-            const ans_preview = translation_preview.translation;
-            const response_preview = { ...INTERACTION_RESPOND_INSTANTLY, data: { ...COMMAND_RESPONSE_DATA, content: `Here is a translation:\n\n\`${ans_preview}\`.\n\nA message will not be sent!` }}
-            return res.status(200).json(response_preview);
+            await DeferSlashCommandResponse(interaction, MessageFlags.Ephemeral);
+            const translation_preview: IBXTranslationObject = await TranslateContent(options!);
+            await EditSlashCommandResponse(interaction, `Here is a translation:\n\n\`${translation_preview.translation}\`.\n\nA message will not be sent!`)
+            return res.status(200).json(ACK_INTERACTION);
+
           default:
             return res.status(404).json(INVALID_COMMAND_RESPONSE);
         }
