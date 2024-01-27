eb9b94e242858f05c1c2673ebfb533c0a2c33984
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2024-01-27 01:37:58 -0600

    Block Command Drafting

--- Bot/src/locales/locales.ts
+++ Bot/src/locales/locales.ts
@@ -4,10 +4,24 @@ export const strings = {
     english_us: 'lang.english_us',
     spanish: 'lang.spanish',
     russian: 'lang.russian',
+    
+    //BLOCK Command
+    blockCommandName: 'cmd.slash.block.title',
+    blockCommandDescription: 'cmd.slash.block.description',
+    blockTypeFieldName: 'cmd.slash.block.options.type.name',
+    blockTypeFieldDescription: 'cmd.slash.block.options.type.description',
+    blockTypeFieldChoiceCustom: 'cmd.slash.block.options.type.choices.custom',
+    blockTypeFieldChoiceProfanity: 'cmd.slash.block.options.type.choices.profanity',
+    blockTypeFieldChoiceSexual: 'cmd.slash.block.options.type.choices.sexual',
+    blockTypeFieldChoiceSlurs: 'cmd.slash.block.options.type.choices.slurs',
+    blockTypeFieldChoiceSpam: 'cmd.slash.block.options.type.choices.spam',
+    
 
-    tanslateThisCommandName: 'cmd.context.translate-this.title',
-    translateThisLangaugeSelectPrompt: 'cmd.context.translate-this.prompt',
-    translateThisLanguageSelectPlaceholder: 'cmd.context.translate-this.placeholder',
+    contributeCommandName: 'cmd.slash.contribute.title',
+    contributeCommandDescription: 'cmd.slash.contribute.description',
+
+    inviteCommandName: 'cmd.slash.invite.title',
+	inviteCommandDescription: 'cmd.slash.invite.description',
 
     translateCommandName: 'cmd.slash.translate.title',
 	translateCommandDescription: 'cmd.slash.translate.description',
@@ -18,9 +32,7 @@ export const strings = {
 	translatePublicFieldName: 'cmd.slash.translate.options.public.name',
 	translatePublicFieldDescription: 'cmd.slash.translate.options.public.description',
 
-    inviteCommandName: 'cmd.slash.invite.title',
-	inviteCommandDescription: 'cmd.slash.invite.description',
-
-    contributeCommandName: 'cmd.slash.contribute.title',
-    contributeCommandDescription: 'cmd.slash.contribute.description',
+    tanslateThisCommandName: 'cmd.context.translate-this.title',
+    translateThisLangaugeSelectPrompt: 'cmd.context.translate-this.prompt',
+    translateThisLanguageSelectPlaceholder: 'cmd.context.translate-this.placeholder',
 };
